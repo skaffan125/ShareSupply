a0e556ae8feacb9f7a2070c1480c476c454dab93
Sheikh Affan <107756008+skaffan125@users.noreply.github.com>
2023-06-12 13:24:16 +0600

    Add files via upload

---
+++ Chat2/php-chat-app-main/index.php
@@ -0,0 +1,106 @@
+<?php 
+  session_start();
+
+  if (!isset($_SESSION['username'])) {
+	$username = $_POST['username'];
+	
+   
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Chat App - Login</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+	<link rel="stylesheet" 
+	      href="css/style.css">
+	<link rel="icon" href="img/logo.png">
+</head>
+<body class="d-flex
+             justify-content-center
+             align-items-center
+             vh-100">
+	 <div class="w-400 p-5 shadow rounded">
+	 <form method="post" 
+	 	      action="app/http/auth.php">
+	 		<div class="d-flex
+	 		            justify-content-center
+	 		            align-items-center
+	 		            flex-column">
+
+	 		<img src="img/logo.png" 
+	 		     class="w-25">
+	 		<h3 class="display-4 fs-1 
+	 		           text-center">
+	 			       LOGIN</h3>   
+
+
+	 		</div>
+	 		<?php if (isset($_GET['error'])) { ?>
+	 		<div class="alert alert-warning" role="alert">
+			  <?php echo htmlspecialchars($_GET['error']);?>
+			</div>
+			<?php } ?>
+			
+	 		<?php if (isset($_GET['success'])) { ?>
+	 		<div class="alert alert-success" role="alert">
+			  <?php echo htmlspecialchars($_GET['success']);?>
+			</div>
+			<?php } ?>
+		  <div class="mb-3">
+		    <label class="form-label">
+		           User name</label>
+		    <input type="text" 
+		           class="form-control"
+		           name="username">
+		  </div>
+
+		  <div class="mb-3">
+		    <label class="form-label">
+		           Password</label>
+		    <input type="password" 
+		           class="form-control"
+		           name="password">
+		  </div>
+		  
+		  <button type="submit" 
+		          class="btn btn-primary">
+		          LOGIN</button>
+				  <a href="../../Recipient/dashboard.php" class="btn btn-dark">Rcipient-Home</a>
+				   <a href="../../Donour/home.php" class="btn btn-dark">Donour-Home</a>
+
+		</form>
+	 </div>
+</body>
+</html>
+
+<?php
+  }else{
+  	header("Location: home.php");
+   	exit;
+  }
+ ?>
+
+I<script>
+  document.addEventListener("DOMContentLoaded", function() {
+    var form = document.querySelector("form");
+
+    // Define the autofill values
+    var autofillData = {
+      username: "<?php echo $username;?>",
+    
+    };
+
+    // Autofill the form
+    form.querySelector("[name='username']").value = autofillData.username;
+    f
+  });
+</script>
+
+
+
+
+
+
